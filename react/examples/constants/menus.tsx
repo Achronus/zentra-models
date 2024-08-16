4b7e3eae5d6943f18fe8897685dde620a78ca8ff
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-08-16 15:53:08 +0100

    Add menu example constants

---
+++ react/examples/constants/menus.tsx
@@ -0,0 +1,96 @@
+import { IconNavLink, NavLink, ProfileMenuItem } from "@/types/core";
+import {
+  Home,
+  LineChart,
+  Package,
+  Settings,
+  ShoppingCart,
+  User,
+  Users,
+} from "lucide-react";
+
+export const NavLinks: NavLink[] = [
+  {
+    url: "/",
+    name: "Dashboard",
+    icon: <Home size={24} />,
+  },
+  {
+    url: "#",
+    name: "Orders",
+    icon: <ShoppingCart size={24} />,
+  },
+  {
+    url: "/dashboard",
+    name: "Products",
+    icon: <Package size={24} />,
+  },
+  {
+    url: "#",
+    name: "Customers",
+    icon: <Users size={24} />,
+  },
+  {
+    url: "#",
+    name: "Analytics",
+    icon: <LineChart size={24} />,
+  },
+];
+
+export const ProfileMenuItems: ProfileMenuItem[][] = [
+  [
+    {
+      type: "label",
+      text: "My Account",
+      icon: <User size={16} />,
+    },
+  ],
+  [
+    {
+      type: "item",
+      text: "Settings",
+      url: "#",
+      icon: <Settings size={16} />,
+    },
+    {
+      type: "item",
+      text: "Support",
+      url: "#",
+    },
+  ],
+  [
+    {
+      type: "item",
+      text: "Logout",
+      url: "#",
+    },
+  ],
+];
+
+export const iconNavLinks: IconNavLink[] = [
+  {
+    url: "/",
+    name: "Dashboard",
+    icon: <Home size={20} />,
+  },
+  {
+    url: "/products",
+    name: "Orders",
+    icon: <ShoppingCart size={20} />,
+  },
+  {
+    url: "#",
+    name: "Products",
+    icon: <Package size={20} />,
+  },
+  {
+    url: "#",
+    name: "Customers",
+    icon: <Users size={20} />,
+  },
+  {
+    url: "#",
+    name: "Analytics",
+    icon: <LineChart size={20} />,
+  },
+];
