ff2f216aeff91c41e8f1d5edf0860e6e982ee7e7
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-08-16 20:23:53 +0100

    Refactor `SearchBox` for `SearchFilter`

--- react/components/zentra/SearchBox.tsx
+++ react/components/zentra/SearchBox.tsx
@@ -1,4 +1,5 @@
 import { Search } from "lucide-react";
+import { ChangeEvent } from "react";
 
 import { Input } from "@/components/ui/input";
 
@@ -6,12 +7,16 @@ type SearchBoxProps = {
   placeholder?: string;
   formStyles?: string;
   inputStyles?: string;
+  value?: string;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
 };
 
 const SearchBox = ({
   placeholder,
   formStyles,
   inputStyles = "pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]",
+  value,
+  onChange,
 }: SearchBoxProps) => {
   return (
     <form className={formStyles}>
@@ -21,6 +26,8 @@ const SearchBox = ({
           type="search"
           placeholder={placeholder ? placeholder : "Search..."}
           className={inputStyles}
+          value={value}
+          onChange={onChange}
         />
       </div>
     </form>
