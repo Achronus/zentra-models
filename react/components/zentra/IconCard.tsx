16e6e7335c94715755d0458d8cdcf5c585d1f05f
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-08-24 20:05:16 +0100

    Added `IconCard` and `IconCardSection` components

---
+++ react/components/zentra/IconCard.tsx
@@ -0,0 +1,117 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+} from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+
+export type IconCardDetails = {
+  title: string;
+  desc: string;
+  icon: React.ReactElement;
+};
+
+export type IconCardStyles = {
+  card?: string;
+  topLine?: string;
+  leftLine?: string;
+  cardHeader?: string;
+  headerContainer?: string;
+  headerWrapper?: string;
+  iconContainer?: string;
+  titleContainer?: string;
+  cardContent?: string;
+  cardDescription?: string;
+};
+
+export type IconCardType = IconCardDetails & {
+  styles?: IconCardStyles;
+};
+
+type IconCardSectionProps = {
+  cards: IconCardType[];
+  className?: string;
+};
+
+type IconCardProps = {
+  title: string;
+  desc: string;
+  icon: React.ReactElement;
+  styles?: IconCardStyles;
+};
+
+export const IconCardSection = ({ cards, className }: IconCardSectionProps) => {
+  return (
+    <section className={cn("wrapper flex gap-8 mt-12 flex-wrap", className)}>
+      {cards.map((card) => (
+        <IconCard key={card.title} {...card} />
+      ))}
+    </section>
+  );
+};
+
+export const IconCard = ({ title, desc, icon, styles }: IconCardProps) => {
+  return (
+    <Card
+      className={cn(
+        "min-w-[300px] relative z-10 flex-1 bg-background",
+        styles?.card
+      )}
+    >
+      <div
+        className={cn(
+          "absolute inset-x-0 inset-y-8 z-[-1] border-t border-slate-800",
+          styles?.topLine
+        )}
+      />
+      <div
+        className={cn(
+          "absolute inset-y-0 inset-x-8 z-[-1] border-l border-slate-800",
+          styles?.leftLine
+        )}
+      />
+      <CardHeader
+        className={cn(
+          "p-4 text-sm font-bold text-slate-300",
+          styles?.cardHeader
+        )}
+      >
+        <div
+          className={cn(
+            "flex items-center rounded-full py-0.5 pr-1 bg-slate-900",
+            styles?.headerContainer
+          )}
+        >
+          <section
+            className={cn("flex gap-2 items-center", styles?.headerWrapper)}
+          >
+            <div
+              className={cn(
+                "rounded-full p-2.5 -m-1 bg-sky-900",
+                styles?.iconContainer
+              )}
+            >
+              {icon}
+            </div>
+            <h3 className={cn("ml-1 font-medium", styles?.titleContainer)}>
+              {title}
+            </h3>
+          </section>
+        </div>
+      </CardHeader>
+      <CardContent
+        className={cn(
+          "flex flex-col gap-2 ml-6 items-start",
+          styles?.cardContent
+        )}
+      >
+        <CardDescription
+          className={cn("text-sm text-slate-400", styles?.cardDescription)}
+        >
+          {desc}
+        </CardDescription>
+      </CardContent>
+    </Card>
+  );
+};
---
+++ react/examples/icon-cards.tsx
@@ -0,0 +1,33 @@
+import { IconCardSection, IconCardType } from "@/components/zentra/IconCard";
+import { Heart, Link, PanelsTopLeft } from "lucide-react";
+
+export const FeaturedCards: IconCardType[] = [
+  {
+    title: "API Craftsman",
+    desc: "Keeping API's clean, efficient, scalable, and maintainable.",
+    icon: <Link size={24} strokeWidth={1.5} />,
+    styles: {
+      iconContainer: "bg-amber-900",
+    },
+  },
+  {
+    title: "Detail Oriented",
+    desc: "Awareness to ease of access, accessibility, and a seamless UX.",
+    icon: <Heart size={24} strokeWidth={1.5} fill="white" />,
+    styles: {
+      iconContainer: "bg-pink-900",
+    },
+  },
+  {
+    icon: <PanelsTopLeft size={24} strokeWidth={1.5} />,
+    title: "Intuitive Interfaces",
+    desc: "Turning JSON data into clear, interactive interfaces.",
+    styles: {
+      iconContainer: "bg-sky-900",
+    },
+  },
+];
+
+export default function IconCardsDemo() {
+  return <IconCardSection cards={FeaturedCards} />;
+}
